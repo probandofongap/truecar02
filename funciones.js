a30e09f552614dc907a75bcd200cb2a11beb5ebe
probandofongap <probandofongap01@gmail.com>
2014-04-01 19:20:58 +0200

    probando
    phonegap

---
+++ funciones.js
@@ -0,0 +1,11 @@
+/**
+* Created with JQueryMobile.
+* User: Jleonp
+* Date: 2014-03-26
+* Time: 03:46 PM
+* To change this template use Tools | Templates.
+*/
+
+window.addEventListener('load',function(){
+	fastClick.attach(document.body);
+});
